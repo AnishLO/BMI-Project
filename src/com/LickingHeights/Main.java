47bc3dad2c272e36ce31d390fb278067b7484592
Anish <Anish.lohar.2022@student.lhschools.org>
2019-09-24 09:14:29 -0400

    Added 6 verbals

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,7 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         //Declare
+        String name;
+        float inches;
+        float meter;
+        float kilograms;
+        float pound;
+        float total;
+
         Scanner keyboard;
+
 
         //Initializing
         keyboard = new Scanner(System.in);
